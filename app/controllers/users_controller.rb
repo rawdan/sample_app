7506a44c5bdc44ea29aa2cb925f80eab488bccc9
rawdan <bogdancrudu@yahoo.com>
2016-05-06 00:42:01 +0300

    Add microposts styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id] || session[:user_id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
